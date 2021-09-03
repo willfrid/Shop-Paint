cb8980de7f1a70dbce4c03132c1caa96664310a5
Dongmo880 <84510058+Dongmo880@users.noreply.github.com>
2021-09-03 08:52:31 +0200

    creation de la page pour les peintures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,14 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class Home1Controller extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/home1", name="home1")
      */
     public function index(PeintureRepository $peintureRepository,BlogpostRepository $blogpostRepository): Response
     {
-        return $this->render('home/index.html.twig',[
+        return $this->render('home1/index.html.twig',[
             'peintures'=>$peintureRepository->lassTree(),
             'blogposts'=>$blogpostRepository->lassTree()
         ]);
