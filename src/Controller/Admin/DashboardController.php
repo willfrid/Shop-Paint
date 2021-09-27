7163031382ad8e986a2af0d3e2e5f97404d767bc
Dongmo880 <84510058+Dongmo880@users.noreply.github.com>
2021-09-27 22:42:25 +0200

    mise a jour du back office pour admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Blogpost;
 use App\Entity\Peinture;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -31,6 +32,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Actualité', 'fas fa-newspaper', Blogpost::class);
         yield MenuItem::linkToCrud('Peinture', 'fas fa-palette', Peinture::class);
+        yield MenuItem::linkToCrud('parametre', 'fas fa-cog', User::class);
 
     }
 }
